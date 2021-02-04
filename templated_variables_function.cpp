bf0462ba8984d7ff33669a36e354f5bc8146a31c
Karvendhan <karvsmech@gmail.com>
2021-02-04 21:10:15 +0530

    filename change

--- templated_variables_function.cpp
+++ templated_variables_function.cpp
@@ -12,6 +12,7 @@ int main() {
     std::cout << "my_max(4, 5): " << my_max(4, 5) << std::endl;
     std::cout << "my_max(\"Hello\", \"World\"): " << my_max(std::string("Hello"), std::string("World")) << std::endl;
     std::cout << "my_max('a', 'd'): " << my_max('a', 'd') << std::endl;
+
     return 0;
 }
 
